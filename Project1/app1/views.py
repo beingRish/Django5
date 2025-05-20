88a2eb2391f19ad305aa668baf85a0e26b8ed0cc
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-05-20 18:12:50 +0530

    Create multiple application in Django

--- Project1/app1/views.py
+++ Project1/app1/views.py
@@ -5,16 +5,5 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('Home Page')
 
-def learn_django(request):
-    return HttpResponse('Hello Django')
-
-def learn_python(request):
-    return HttpResponse('<h1>Learn Python</h1>')
-
-def learn_math(request):
-    a = 10 + 20
-    return HttpResponse(a)
-
-def learn_php(request):
-    lang = '<h1>Hello PHP</h1>'
-    return HttpResponse(lang)
+def myapp1(request):
+    return HttpResponse("My App 1 Page")
--- Project1/app2/views.py
+++ Project1/app2/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def myapp2(request):
+    return HttpResponse("My App 2 Page")
+
+def myapp2_me(request):
+    return HttpResponse("My App 2 Me Page")
