81daa34b97b34ad104a9c748d4d91327660bace9
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-05-21 00:41:38 +0530

    Render Dynamic Template in Django

--- Project1/course/views.py
+++ Project1/course/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def learn_django(req):
-    return render(req,'course/django.html')
+    coursename = {'cname': 'Django 5.1'}
+    return render(req, 'course/django.html', coursename)
 
 def learn_fastapi(req):
-    return render(req, 'course/fastapi.html')
+    seats = 10
+    coursedetails = {
+        'cname': 'FAST API',
+        'version': '3.1',
+        'st':seats
+    }
+
+    return render(req, 'course/fastapi.html', coursedetails)
