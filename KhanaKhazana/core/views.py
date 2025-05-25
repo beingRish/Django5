fda80ba62e5563447381bd43a2d87f4e39d189c8
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-05-25 16:23:25 +0530

    Completed Khana Khazana App

--- KhanaKhazana/core/views.py
+++ KhanaKhazana/core/views.py
@@ -21,3 +21,15 @@ def home(req):
         },
     ]
     return render(req, 'core/home.html', {'dishes': dishes})
+
+def menu(req):
+    return render(req, 'core/menu.html')
+
+def tracking(req):
+    return render(req, 'core/tracking.html')
+
+def reservation(req):
+    return render(req, 'core/reservation.html')
+
+def contact(req):
+    return render(req, 'core/contact.html')
