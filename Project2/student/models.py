2a7a3f25269eef6cb8b9d17dccf8e62637ace718
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-05-25 17:22:22 +0530

    Model and Create Database Table in Django 5

--- Project2/student/models.py
+++ Project2/student/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name=models.CharField(max_length=70)
+    email=models.EmailField(max_length=255)
+    city=models.CharField(max_length=70)
+    roll=models.IntegerField()
+    state=models.CharField(max_length=70)
+    comment=models.CharField(max_length=70, default='nothing')
+
+    # python manage.py makemigrations
+    # python manage.py migrate
+    # python manage.py showmigrations
+    # python manage.py sqlmigrate <app_name> <migrations_file_name>
