459bb11059963957e943be9746bc3c1e96ed6fda
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-05-26 18:18:16 +0530

    Django Model Admin Display Database Data in Admin Panel

--- Project2/student/admin.py
+++ Project2/student/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from student.models import Profile, Result
 
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(Result)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'roll', 'email', 'city']
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['id', 'stu_class', 'marks']
--- Project2/student/models.py
+++ Project2/student/models.py
@@ -1,23 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class Profile(models.Model):
     name=models.CharField(max_length=70)
     roll=models.IntegerField()
     email=models.EmailField(max_length=255)
     city=models.CharField(max_length=70)
 
-    def __str__(self):
-        return self.name
-
 class Result(models.Model):
     stu_class=models.CharField(max_length=70)
     marks=models.IntegerField()
-
-    def __str__(self):
-        return self.stu_class
-
-    # python manage.py makemigrations
-    # python manage.py migrate
-    # python manage.py showmigrations
-    # python manage.py sqlmigrate <app_name> <migrations_file_name>
