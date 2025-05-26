66b89122b9084e389c17b5977d3ee62032509f92
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-05-26 17:43:13 +0530

    Register Model Class to Show in Admin Panel Django 5

--- Project2/student/models.py
+++ Project2/student/models.py
@@ -3,11 +3,19 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     name=models.CharField(max_length=70)
+    roll=models.IntegerField()
     email=models.EmailField(max_length=255)
     city=models.CharField(max_length=70)
-    roll=models.IntegerField()
-    state=models.CharField(max_length=70)
-    comment=models.CharField(max_length=70, default='nothing')
+
+    def __str__(self):
+        return self.name
+
+class Result(models.Model):
+    stu_class=models.CharField(max_length=70)
+    marks=models.IntegerField()
+
+    def __str__(self):
+        return self.stu_class
 
     # python manage.py makemigrations
     # python manage.py migrate
