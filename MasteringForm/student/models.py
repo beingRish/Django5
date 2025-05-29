3d08906a4a3f13ec44d4ee3d8ab4aee1f1171077
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-05-29 17:26:36 +0530

    Created template and all

--- MasteringForm/student/models.py
+++ MasteringForm/student/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+
+def validate_pin_length(value):
+    if len(str(value)) != 6:
+        raise ValidationError('The PIN code must be exactly 6 digits.')
+    
+STATE_CHOICE = (
+    ("Andaman and Nicobar Islands", "Andaman and Nicobar Islands"),
+    ("Andhra Pradesh", "Andhra Pradesh"),
+    ("Arunachal Pradesh", "Arunachal Pradesh"),
+    ("Assam", "Assam"),
+    ("Bihar", "Bihar"),
+    ("Chandigarh", "Chandigarh"),
+    ("Chhattisgarh", "Chhattisgarh"),
+    ("Dadra and Nagar Haveli and Daman and Diu", "Dadra and Nagar Haveli and Daman and Diu"),
+    ("Delhi", "Delhi"),
+    ("Goa", "Goa"),
+    ("Gujarat", "Gujarat"),
+    ("Haryana", "Haryana"),
+    ("Himachal Pradesh", "Himachal Pradesh"),
+    ("Jammu and Kashmir", "Jammu and Kashmir"),
+    ("Jharkhand", "Jharkhand"),
+    ("Karnataka", "Karnataka"),
+    ("Kerala", "Kerala"),
+    ("Ladakh", "Ladakh"),
+    ("Lakshadweep", "Lakshadweep"),
+    ("Madhya Pradesh", "Madhya Pradesh"),
+    ("Maharashtra", "Maharashtra"),
+    ("Manipur", "Manipur"),
+    ("Meghalaya", "Meghalaya"),
+    ("Mizoram", "Mizoram"),
+    ("Nagaland", "Nagaland"),
+    ("Odisha", "Odisha"),
+    ("Puducherry", "Puducherry"),
+    ("Punjab", "Punjab"),
+    ("Rajasthan", "Rajasthan"),
+    ("Sikkim", "Sikkim"),
+    ("Tamil Nadu", "Tamil Nadu"),
+    ("Telangana", "Telangana"),
+    ("Tripura", "Tripura"),
+    ("Uttar Pradesh", "Uttar Pradesh"),
+    ("Uttarakhand", "Uttarakhand"),
+    ("West Bengal", "West Bengal")
+)
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    dob = models.DateField(auto_now=False, auto_now_add=False)
+    gender = models.CharField(max_length=1)
+    locality = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    pin = models.PositiveIntegerField(
+        validators=[validate_pin_length],
+        help_text='Enter 6-digit pin code'
+    )
+    state = models.CharField(choices=STATE_CHOICE, max_length=100)
+    mobile = models.CharField(
+        max_length=100,
+        validators=[RegexValidator(regex=r'^\d{10}$')],
+        help_text='Enter a 10-digit mobile number'
+    )
+    email = models.EmailField()
+    job_city = models.CharField(max_length=50)
+    profile_image = models.ImageField(upload_to='doc', blank=True)
