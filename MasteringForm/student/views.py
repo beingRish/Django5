6ac1ac5490f41e75637dadbe0d0bc3d2dadaba1b
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-05-30 00:11:20 +0530

    Successfully stored form data in database

--- MasteringForm/student/views.py
+++ MasteringForm/student/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from student.forms import ProfileForm
 from student.models import Profile
 
 def home(request):
-    form = ProfileForm()
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ProfileForm()
     return render(request, 'student/home.html', {'form': form})
