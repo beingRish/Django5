aef41b5ff7e95927048e316990187eae92d0bc2e
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-02 17:55:53 +0530

    Registration view completed

--- AuthenticationAndAuthorization/account/views.py
+++ AuthenticationAndAuthorization/account/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from account.forms import RegistrationForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -10,7 +12,21 @@ def login_view(request):
     return render(request, 'account/login.html')
 
 def register_view(request):
-    return render(request, 'account/register.html')
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.is_active = False
+            user.save()
+            messages.success(
+                request,
+                'Registration successful! Please check your email to activate your account.',
+            )
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'account/register.html', {'form': form})
 
 def password_reset_view(request):
     return render(request, 'account/password_reset.html')
