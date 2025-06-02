85fcb21a894b05a490ca66a2be08690a0134050e
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-02 16:38:48 +0530

    User and User Manager Models Created

--- AuthenticationAndAuthorization/AuthenticationAndAuthorization/settings.py
+++ AuthenticationAndAuthorization/AuthenticationAndAuthorization/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# To Use Custom User Model
+AUTH_USER_MODEL = 'account.User'
