7956c12765a5223fa59d030a7fdb04e38b75f3b9
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-02 16:39:47 +0530

    Deleted 'Authentication and Permissions' Project

--- AuthenticationAndPermission/customer/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def customer_dasboard_view(request):
-    return render(request, 'customer/dashboard.html')
-
-def password_change_view(request):
-    return render(request, 'customer/password_change.html')
--- AuthenticationAndPermission/seller/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from seller.views import seller_dasboard_view
-
-urlpatterns = [
-    path('dashboard', seller_dasboard_view, name='seller_dashboard'),
-    
-]
