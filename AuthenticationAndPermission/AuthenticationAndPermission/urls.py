cb6abb2b33319626c4cda0e90aac42b753b0a8e3
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-02 11:45:50 +0530

    account section completed

--- AuthenticationAndPermission/AuthenticationAndPermission/urls.py
+++ AuthenticationAndPermission/AuthenticationAndPermission/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
-    path('customer/', include('customers.urls')),
-    path('seller/', include('seller.urls')),
+    # path('customer/', include('customers.urls')),
+    # path('seller/', include('seller.urls')),
 ]
--- AuthenticationAndPermission/account/views.py
+++ AuthenticationAndPermission/account/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render, redirect
 def home(request):
     return render(request, 'account/home.html')
 
+def login_view(request):
+    if request.method == "POST":
+        return redirect('customer_dashboard')
+    return render(request, 'account/login.html')
+
+def register_view(request):
+    return render(request, 'account/register.html')
+
+def password_reset_view(request):
+    return render(request, 'account/password_reset.html')
+
+def password_reset_confirm_view(request):
+    return render(request, 'account/password_reset_confirm.html')
