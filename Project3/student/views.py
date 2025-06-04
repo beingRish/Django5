83d228b5dc7bea63549c615fba05a734f390b4fd
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-05 00:53:14 +0530

    Completed Cookies in Django5

--- Project3/student/views.py
+++ Project3/student/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from datetime import datetime, timedelta, timezone
 
-# Create your views here.
+def setcookie(request):
+    response = render(request, 'student/setcookie.html')
+    response.set_cookie('pay_id', 'ppp123')
+    # response.set_cookie('pay_id', 'ppp123', max_age=3600)
+    # response.set_cookie('pay_id', 'ppp123', expires=datetime.now(timezone.utc) + timedelta(days=2))
+    return response
+
+
+def getcookie(request):
+    # pay_id = request.COOKIES['pay_id']
+    pay_id = request.COOKIES.get('pay_id', 'default_pay_id123')
+    return render(request, 'student/getcookie.html', {'pay_id': pay_id})
+
+
+def delcookie(request):
+    response = render(request, 'student/delcookie.html')
+    response.delete_cookie('pay_id')
+    return response
+
+
+def setsignedcookie(request):
+    response = render(request, 'student/setsignedcookie.html')
+    response.set_signed_cookie('token', 't123456', salt='tk')
+    return response
+
+
+def getsignedcookie(request):
+    token = request.get_signed_cookie('token', default="guest_toke123", salt='tk')
+    return render(request, 'student/getsignedcookie.html', {'token': token})
---
+++ README.md
@@ -0,0 +1,11 @@
+HttpResponse.set_cookie(
+    key,
+    value='',
+    max_age=None,   // max_age = 3600
+    expires=None,
+    path='/',
+    domain=None,
+    secure=False,
+    httponly=False,
+    samesite=None
+)
