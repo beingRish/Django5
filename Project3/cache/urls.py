25969b4af9a3e8e55e1d22678a459eecd301b1ba
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-06 13:48:52 +0530

    Per View Caching in Django 5

--- Project3/cache/urls.py
+++ Project3/cache/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from cache.views import course
+from cache.views import home, course, result
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
+    path('', home, name="home"),
+    path('home', cache_page(60)(home), name="home"),
+    path('index', cache_page(90)(home), name="home"),
     path('course/', course, name="course"),
+    path('result/', cache_page(30)(result), name="result"),
 ]
--- Project3/cache/views.py
+++ Project3/cache/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
+def home(request):
+    return render(request, 'cache/home.html')
+
+
+@cache_page(30)
 def course(request):
     return render(request, 'cache/course.html')
+
+
+def result(request):
+    return render(request, 'cache/result.html')
