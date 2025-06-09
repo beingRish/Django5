21b9d3ec59aa312e0cc03311db40f240bde71463
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-09 20:33:38 +0530

    Created Models and Middlewares

--- UnderConstruction/uc/middlewares.py
+++ UnderConstruction/uc/middlewares.py
@@ -1,11 +1,12 @@
+from django.shortcuts import render
+
 class UnderConstructionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        print("One Time Initialization")
 
     def __call__(self, request):
         print("This is before view")
-        response = self.get_response(request)
-        # response = render(request, 'uc/under.html')
+        # response = self.get_response(request)
+        response = render(request, 'uc/underc.html')
         print("This is after view")
         return response
