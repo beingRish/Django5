c3039f205d9bfcf7343ffb3096bbe56d3eeb8e9f
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-13 01:17:02 +0530

    Django Model Relationship Practical Example

--- ModelRelationship/myapp/admin.py
+++ ModelRelationship/myapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Page, Post, Song
 
-# Register your models here.
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['pname', 'pcat', 'ppublished', 'user']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'ptcat', 'ptpublished', 'user']
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ['stitle', 'sduration', 'written_by']
+
