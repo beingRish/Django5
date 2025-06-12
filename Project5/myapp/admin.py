ab0e34fd2679b93fe89c53a41015c9382275d855
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-12 20:32:53 +0530

    Django Model One to One Relationship

---
+++ Project5/myapp/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from myapp.models import Profile, Page, Like
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['name', 'city']
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['page_name']
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['likes']
