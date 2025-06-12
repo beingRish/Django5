b98d8c6a9d955924f5b2a0b272a7ca578fc8a1dc
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-12 16:45:46 +0530

    Working with Database Aggregate Function

--- Project4/school/views.py
+++ Project4/school/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from school.models import Student, Teacher
-from django.db.models import Q
+from django.db.models import Avg, Sum, Min, Max, Count
 from datetime import date, time
 
 # # This home view is for 'Working with Database Returns New Queryset'
@@ -139,58 +139,90 @@ from datetime import date, time
 
 
 
-# This home view is for 'Working with Database Field Lookups
-def home(request):
-    # student_data = Student.objects.all()
-    # student_data = Student.objects.filter(name__exact='Sonam')  # case sensitive
-    # student_data = Student.objects.filter(name__iexact='Sonam')  #case ensensitive
-    # student_data = Student.objects.filter(name__contains='S')  # case sensitive -> might not work with sqlite but work with mySQL, and PostgreSQL
-    # student_data = Student.objects.filter(name__icontains='i')  # case ensensitive
-    # student_data = Student.objects.filter(id__in=[1, 3, 7])
-    # student_data = Student.objects.filter(marks__gt=200)
-    # student_data = Student.objects.filter(marks__gte=200)
-    # student_data = Student.objects.filter(marks__lt=200)
-    # student_data = Student.objects.filter(name__startswith='S')
-    # student_data = Student.objects.filter(name__istartswith='S')
-    # student_data = Student.objects.filter(name__endswith='t')
-    # student_data = Student.objects.filter(name__iendswith='t')
-    # student_data = Student.objects.filter(pass_date__range=('2024-01-01', '2024-12-24'))
-    # student_data = Student.objects.filter(pass_date__range=('2024-01-01', '2024-12-24'))
-    # student_data = Student.objects.filter(admission_date__date=date(2023, 8, 15))
-    # student_data = Student.objects.filter(admission_date__date__gt=date(2023, 8, 15))
-    # student_data = Student.objects.filter(admission_date__date__lt=date(2023, 8, 15))
-    # student_data = Student.objects.filter(pass_date__year=2023)
-    # student_data = Student.objects.filter(pass_date__year__gt=2023)
-    # student_data = Student.objects.filter(pass_date__year__lt=2023)
-    # student_data = Student.objects.filter(pass_date__month=12)
-    # student_data = Student.objects.filter(pass_date__month__gt=5)
-    # student_data = Student.objects.filter(pass_date__month__lt=5)
-    # student_data = Student.objects.filter(pass_date__day=4)
-    # student_data = Student.objects.filter(pass_date__day__gt=4)
-    # student_data = Student.objects.filter(pass_date__day__lt=4)
-    # student_data = Student.objects.filter(pass_date__week=15)
-    # student_data = Student.objects.filter(pass_date__week__gt=15)
-    # student_data = Student.objects.filter(pass_date__week__lt=15)
-    # student_data = Student.objects.filter(pass_date__week_day=4)
-    # student_data = Student.objects.filter(pass_date__week_day__gt=4)
-    # student_data = Student.objects.filter(pass_date__week_day__lt=4)
-    # student_data = Student.objects.filter(pass_date__quarter=1)
-    # student_data = Student.objects.filter(pass_date__quarter__gt=2)
-    # student_data = Student.objects.filter(pass_date__quarter__lt=2)
-    # student_data = Student.objects.filter(admission_date__time=time(6,00))
-    # student_data = Student.objects.filter(admission_date__time__gt=time(10,00))
-    # student_data = Student.objects.filter(admission_date__time__lt=time(10,00))
-    # student_data = Student.objects.filter(admission_date__hour=6)
-    # student_data = Student.objects.filter(admission_date__hour__gt=10)
-    # student_data = Student.objects.filter(admission_date__hour__lt=10)
-    # student_data = Student.objects.filter(admission_date__minute=57)
-    # student_data = Student.objects.filter(admission_date__minute__gt=40)
-    # student_data = Student.objects.filter(admission_date__minute__lt=40)
-    # student_data = Student.objects.filter(admission_date__second=57)
-    # student_data = Student.objects.filter(admission_date__second__gt=40)
-    # student_data = Student.objects.filter(admission_date__second__lt=40)
-    student_data = Student.objects.filter(roll__isnull=False)
+# # This home view is for 'Working with Database Field Lookups
+# def home(request):
+#     # student_data = Student.objects.all()
+#     # student_data = Student.objects.filter(name__exact='Sonam')  # case sensitive
+#     # student_data = Student.objects.filter(name__iexact='Sonam')  #case ensensitive
+#     # student_data = Student.objects.filter(name__contains='S')  # case sensitive -> might not work with sqlite but work with mySQL, and PostgreSQL
+#     # student_data = Student.objects.filter(name__icontains='i')  # case ensensitive
+#     # student_data = Student.objects.filter(id__in=[1, 3, 7])
+#     # student_data = Student.objects.filter(marks__gt=200)
+#     # student_data = Student.objects.filter(marks__gte=200)
+#     # student_data = Student.objects.filter(marks__lt=200)
+#     # student_data = Student.objects.filter(name__startswith='S')
+#     # student_data = Student.objects.filter(name__istartswith='S')
+#     # student_data = Student.objects.filter(name__endswith='t')
+#     # student_data = Student.objects.filter(name__iendswith='t')
+#     # student_data = Student.objects.filter(pass_date__range=('2024-01-01', '2024-12-24'))
+#     # student_data = Student.objects.filter(pass_date__range=('2024-01-01', '2024-12-24'))
+#     # student_data = Student.objects.filter(admission_date__date=date(2023, 8, 15))
+#     # student_data = Student.objects.filter(admission_date__date__gt=date(2023, 8, 15))
+#     # student_data = Student.objects.filter(admission_date__date__lt=date(2023, 8, 15))
+#     # student_data = Student.objects.filter(pass_date__year=2023)
+#     # student_data = Student.objects.filter(pass_date__year__gt=2023)
+#     # student_data = Student.objects.filter(pass_date__year__lt=2023)
+#     # student_data = Student.objects.filter(pass_date__month=12)
+#     # student_data = Student.objects.filter(pass_date__month__gt=5)
+#     # student_data = Student.objects.filter(pass_date__month__lt=5)
+#     # student_data = Student.objects.filter(pass_date__day=4)
+#     # student_data = Student.objects.filter(pass_date__day__gt=4)
+#     # student_data = Student.objects.filter(pass_date__day__lt=4)
+#     # student_data = Student.objects.filter(pass_date__week=15)
+#     # student_data = Student.objects.filter(pass_date__week__gt=15)
+#     # student_data = Student.objects.filter(pass_date__week__lt=15)
+#     # student_data = Student.objects.filter(pass_date__week_day=4)
+#     # student_data = Student.objects.filter(pass_date__week_day__gt=4)
+#     # student_data = Student.objects.filter(pass_date__week_day__lt=4)
+#     # student_data = Student.objects.filter(pass_date__quarter=1)
+#     # student_data = Student.objects.filter(pass_date__quarter__gt=2)
+#     # student_data = Student.objects.filter(pass_date__quarter__lt=2)
+#     # student_data = Student.objects.filter(admission_date__time=time(6,00))
+#     # student_data = Student.objects.filter(admission_date__time__gt=time(10,00))
+#     # student_data = Student.objects.filter(admission_date__time__lt=time(10,00))
+#     # student_data = Student.objects.filter(admission_date__hour=6)
+#     # student_data = Student.objects.filter(admission_date__hour__gt=10)
+#     # student_data = Student.objects.filter(admission_date__hour__lt=10)
+#     # student_data = Student.objects.filter(admission_date__minute=57)
+#     # student_data = Student.objects.filter(admission_date__minute__gt=40)
+#     # student_data = Student.objects.filter(admission_date__minute__lt=40)
+#     # student_data = Student.objects.filter(admission_date__second=57)
+#     # student_data = Student.objects.filter(admission_date__second__gt=40)
+#     # student_data = Student.objects.filter(admission_date__second__lt=40)
+#     student_data = Student.objects.filter(roll__isnull=False)
     
 
+#     print("Return: ", student_data)
+#     return render(request, 'school/home.html', {'students': student_data})
+
+
+
+
+
+
+
+
+# This home view is for 'Working with Database Aggregate Function'
+def home(request):
+    student_data = Student.objects.all()
+    average = student_data.aggregate(Avg('marks'))
+    total = student_data.aggregate(Sum('marks'))
+    minimum = student_data.aggregate(Min('marks'))
+    maximum = student_data.aggregate(Max('marks'))
+    totalCount = student_data.aggregate(Count('marks'))
+
+
+
+
+
+    context = {
+        'students': student_data,
+        'average': average,
+        'total': total,
+        'minimum': minimum,
+        'maximum': maximum,
+        'totalCount': totalCount,
+    }
+    
     print("Return: ", student_data)
-    return render(request, 'school/home.html', {'students': student_data})
+    return render(request, 'school/home.html', context)
