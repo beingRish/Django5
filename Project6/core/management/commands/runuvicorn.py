b535c951d98739f48cf5eacfedcb91741e8f49fe
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-13 11:28:29 +0530

    Setup Django Async Server ASGI using Uvicorn

---
+++ Project6/core/management/commands/runuvicorn.py
@@ -0,0 +1,7 @@
+from django.core.management.base import BaseCommand
+import uvicorn
+
+class Command(BaseCommand):
+    help = "Run the Django Application with Uvicorn"
+    def handle(self, *args, **options):
+        uvicorn.run("Project6.asgi:application", host="127.0.0.1", port=8000, reload=True)
