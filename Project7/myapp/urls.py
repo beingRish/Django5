85b35465bed8db72d42912c828d7aaca9a27c55a
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-14 00:35:38 +0530

    Class Based View | Class Based View in Django

--- Project7/myapp/urls.py
+++ Project7/myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from myapp.views import myfunview1, myfunview2, homefunview, aboutfunview, newsfunview, contactfunview
+from myapp.views import myfunview1, myfunview2, homefunview, aboutfunview, newsfunview, contactfunview, MyClassView1, MyClassView2, MyClassView3, MyChildClassView3, HomeClassView, AboutClassView, NewsClassView, ContactClassView
 
 urlpatterns = [
+    # Urls for Function Based View
     path('fun1/', myfunview1, name='myfunview1'),
     path('fun2/', myfunview2, name='myfunview2'),
     path('homefun/', homefunview, name='home_fun_view'),
@@ -10,4 +11,17 @@ urlpatterns = [
     path('newsfun/', newsfunview, {'template_name':'myapp/news.html'}, name='news_fun_view'),
     path('newsfun2/', newsfunview, {'template_name':'myapp/news2.html'}, name='news_fun2_view'),
     path('contactfun/', contactfunview, name='contact_fun_view'),
+
+    # Urls for Class Based View
+    path('cl1/', MyClassView1.as_view(), name='myclassview1'),
+    path('cl2/', MyClassView2.as_view(), name='myclassview2'),
+    # path('cl3/', MyClassView3.as_view(), name='myclassview3'),
+    path('cl3/', MyClassView3.as_view(name='Ritik'), name='myclassview3'),
+    path('chcl3/', MyChildClassView3.as_view(), name='mychildclassview3'),
+    path('homecl/', HomeClassView.as_view(), name='home_class_view'),
+    path('aboutcl/', AboutClassView.as_view(), name='about_class_view'),
+    path('newscl/', NewsClassView.as_view(template_name='myapp/news.html'), name='newscl'),
+    path('newscl2/', NewsClassView.as_view(template_name='myapp/news2.html'), name='newscl2'),
+    path('contactcl/', ContactClassView.as_view(), name='contact_class_view'),
+
 ]
