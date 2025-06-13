56b1bccb0e3777d0f206d18510df648b319484b5
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-13 17:29:20 +0530

    Django Async View Difference between Sync and Async View in Django 5

--- Project6/myapp/urls.py
+++ Project6/myapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from myapp.views import home, product, contact
+from myapp.views import home, product, contact, sync_view, async_view
 
 urlpatterns = [
     path('', home, name='home'),
     path('product/', product, name='product'),
     path('contact/', contact, name='contact'),
+    path('sync/', sync_view, name='sync_view'),
+    path('async/', async_view, name='async_view'),
 ]
--- Project6/myapp/views.py
+++ Project6/myapp/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+import httpx
+import time
+import asyncio
 
 def home(request):
     return render(request, 'myapp/home.html')
@@ -11,3 +15,37 @@ def product(request):
 def contact(request):
     return render(request, 'myapp/contact.html')
 
+
+#Synchronous View
+def sync_view(request):
+    start_time = time.time()
+    responses = []
+    for _ in range(5):
+        response = httpx.get("https://jsonplaceholder.typicode.com/posts")
+        responses.append(response.json())
+
+    end_time = time.time()
+    time_taken = end_time - start_time
+    return JsonResponse({
+        'status': 'Success',
+        'total_request': 5,
+        'time_taken': f"{time_taken:.2f} seconds"
+    })
+
+
+#Asynchronous View
+async def async_view(request):
+    start_time = time.time()
+
+    async with httpx.AsyncClient() as client:
+        tasks = [client.get("https://jsonplaceholder.typicode.com/posts") for _ in range(5)]
+        responses = await asyncio.gather(*tasks)
+
+    
+    end_time = time.time()
+    time_taken = end_time - start_time
+    return JsonResponse({
+        'status': 'Success',
+        'total_request': 5,
+        'time_taken': f"{time_taken:.2f} seconds"
+    })
