7a53f480d0c0975c48fcb4f93e255c3f536184cc
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-15 00:34:26 +0530

    Generic Class Based View | ListView

--- Project8/myapp/urls.py
+++ Project8/myapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from myapp.views import AllStudentView
+from myapp.views import AllStudentView, StudentListView, StudentListView1, StudentListView2, StudentListView3
 
 urlpatterns = [
     path('', AllStudentView.as_view(), name='all_student'),
+    path('students/', StudentListView.as_view(), name='students'),
+    path('students1/', StudentListView1.as_view(), name='students1'),
+    path('students2/', StudentListView2.as_view(), name='students2'),
+    path('students3/', StudentListView3.as_view(), name='students3'),
 ]
--- Project8/myapp/views.py
+++ Project8/myapp/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
 from django.views import View
 from myapp.models import Student
+from django.views.generic.list import ListView
 
 class AllStudentView(View):
     def get(self, request):
         all_students = Student.objects.all()
         return render(request, 'myapp/all_student.html', {'all_students': all_students})
+    
+class StudentListView(ListView):
+    model = Student
+    
+class StudentListView1(ListView):
+    model = Student
+    template_name_suffix = '_all'
+    ordering=['name']
+    
+class StudentListView2(ListView):
+    model = Student
+    template_name = 'myapp/students.html'
+    context_object_name = 'students'
+    
+class StudentListView3(ListView):
+    model = Student
+    template_name = 'myapp/sabhistudents.html'
+    context_object_name = 'students'
+
+    def get_queryset(self):
+        return Student.objects.filter(course='Python')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['django_students'] = Student.objects.filter(course='Django')
+        return context
+    
+    def get_template_names(self):
+        if self.request.COOKIES.get('user') == 'sonam':
+            template_name = 'myapp/sonam.html'
+        else:
+            template_name = self.template_name
+        return [template_name]
