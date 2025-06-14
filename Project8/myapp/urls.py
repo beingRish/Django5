05d1bee56e5b0ba5cd4dbce07369cfb86d571256
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-15 01:09:00 +0530

    Generic Class Based View | DetailView

--- Project8/myapp/urls.py
+++ Project8/myapp/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
-from myapp.views import AllStudentView, StudentListView, StudentListView1, StudentListView2, StudentListView3
+from myapp.views import AllStudentView, SingleStudentView, StudentListView, StudentListView1, StudentListView2, StudentListView3, StudentDetailView, StudentDetailView1, StudentDetailView2, StudentDetailView3
 
 urlpatterns = [
     path('', AllStudentView.as_view(), name='all_student'),
+    path('<int:pk>/', SingleStudentView.as_view(), name='single_student'),
+
+    # URLs for ListView
     path('students/', StudentListView.as_view(), name='students'),
     path('students1/', StudentListView1.as_view(), name='students1'),
     path('students2/', StudentListView2.as_view(), name='students2'),
     path('students3/', StudentListView3.as_view(), name='students3'),
+
+    # URLs for DetailView
+    path('student/<int:pk>/', StudentDetailView.as_view(), name='studentdetail'),
+    path('student1/<int:my_id>/', StudentDetailView1.as_view(), name='studentdetail1'),
+    path('student2/<int:pk>/', StudentDetailView2.as_view(), name='studentdetail2'),
+    path('student3/<int:pk>/', StudentDetailView3.as_view(), name='studentdetail3'),
+
+
 ]
--- Project8/myapp/views.py
+++ Project8/myapp/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 from django.views import View
 from myapp.models import Student
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 class AllStudentView(View):
     def get(self, request):
         all_students = Student.objects.all()
         return render(request, 'myapp/all_student.html', {'all_students': all_students})
     
+class SingleStudentView(View):
+    def get(self, request, pk):
+        single_student = Student.objects.get(pk=pk)
+        return render(request, 'myapp/single_student.html', {'single_student': single_student})
+    
+
+# List View
+
 class StudentListView(ListView):
     model = Student
     
@@ -40,3 +49,30 @@ class StudentListView3(ListView):
         else:
             template_name = self.template_name
         return [template_name]
+    
+
+# Detail View
+
+class StudentDetailView(DetailView):
+    model = Student
+
+class StudentDetailView1(DetailView):
+    model = Student
+    pk_url_kwarg = 'my_id'
+    # template_name_suffix = '_jankari'
+    template_name = 'myapp/student.html'
+
+class StudentDetailView2(DetailView):
+    model = Student
+    template_name = 'myapp/student2.html'
+    context_object_name = 'stu'
+
+class StudentDetailView3(DetailView):
+    model = Student
+    template_name = 'myapp/student3.html'
+    context_object_name = 'stu'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['students'] = Student.objects.all().order_by('name')
+        return context
