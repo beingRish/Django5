ca774f2c41f8a1f6e1571b6fec94b700aa1240cf
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-06-16 18:41:10 +0530

    Complete Django Custom Authentication System using CBV

--- AuthenticationAndAuthorizationCBV/account/admin.py
+++ AuthenticationAndAuthorizationCBV/account/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from account.models import User
 
 # Register your models here.
+class UserModelAdmin(UserAdmin):
+    model = User
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserModelAdmin
+    # That reference specific fields on auth.User.
+    list_display = ["id", "email", "name", "is_active", "is_superuser", "is_staff", "is_customer", "is_seller"]
+    list_filter = ["is_superuser"]
+    fieldsets = [
+        ("User Credentials", {"fields": ["email", "password"]}),
+        ("Personal Information", {"fields": ["name", "city"]}),
+        ("Permissions", {"fields": ["is_active", "is_staff", "is_superuser", "is_customer", "is_seller"]}),
+    ]
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "password1", "password2"],
+
+            },
+        ),
+    ]
+    search_fields = ["email"]
+    ordering = ["email", "id"]
+    # filter_horizontal = ["groups", "user_permissions"]
+    filter_horizontal = []
+
+# Now register the new UserModelAdmin
+admin.site.register(User, UserModelAdmin)
--- AuthenticationAndAuthorizationCBV/seller/views.py
+++ AuthenticationAndAuthorizationCBV/seller/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class SellerDashboardView(View):
+class SellerDashboardView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         return render(request, 'seller/dashboard.html')
